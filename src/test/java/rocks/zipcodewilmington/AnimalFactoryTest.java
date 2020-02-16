0a9bc84a3bff01f22511389d95f34d4b700325b7
Moe Aydin <maydin@zipcodes-MacBook-Pro-23.local>
2020-02-15 22:30:44 -0500

    finished TDD lab

--- src/test/java/rocks/zipcodewilmington/AnimalFactoryTest.java
+++ src/test/java/rocks/zipcodewilmington/AnimalFactoryTest.java
@@ -1,9 +1,51 @@
 package rocks.zipcodewilmington;
 
+import org.junit.Assert;
+import org.junit.Test;
+import rocks.zipcodewilmington.animals.Cat;
+import rocks.zipcodewilmington.animals.Dog;
+import rocks.zipcodewilmington.animals.animal_creation.AnimalFactory;
+
+import java.util.Date;
+
 /**
  * @author leon on 4/19/18.
  */
 public class AnimalFactoryTest {
     //TODO - Create Test for `Animal createDog(String name, Date birthDate)`
+    @Test
+    public void testCreateDog() {
+
+        // Given
+        AnimalFactory animalCreator = new AnimalFactory();
+        String givenName = "Cooper";
+        Date givenBirthDate = new Date(1234);
+
+        // When
+        Dog cooper = animalCreator.createDog(givenName, givenBirthDate);
+
+        // Then
+        Assert.assertEquals(givenName, cooper.getName());
+        Assert.assertEquals(givenBirthDate, cooper.getBirthDate());
+
+    }
+
     //TODO - Create Test for `Animal createCat(String name, Date birthDate)`
+    @Test
+    public void testCreateCat() {
+
+        // Given
+        AnimalFactory animalCreator = new AnimalFactory();
+        String givenName = "Shadow";
+        Date givenBirthDate = new Date(76);
+
+        // When
+        Cat shadow = animalCreator.createCat(givenName, givenBirthDate);
+
+        // Then
+        Assert.assertEquals(givenName, shadow.getName());
+        Assert.assertEquals(givenBirthDate, shadow.getBirthDate());
+
+    }
+
 }
